57272262a5d1ffa4cb1edba9e84de6d708199e59
Profe: 'Fer Luna <127750794+FerMoon@users.noreply.github.com>
2023-05-16 18:57:48 -0300

    Ejemplos clase 01

---
+++ Clase 01 - Terminado/js/index.js
@@ -0,0 +1,39 @@
+/* 
+Comentarios en el código JS
+son iguales que para HTML y CSS
+ */
+
+//con datos del tipo STRING = cadena de texto
+let empresa = 'Coderhouse'
+// let nombreCompleto = 'Fer Moon'
+let password = "Fer.Moon-2111"
+
+//con datos del tipo numérico
+let anioActual = 2023
+let nroDeLaSuerte = 7
+// let nombre_completo = "Fernando Luna"
+let numA = 21
+let numB = 75
+
+const userId = 2101
+
+//con datos del tipo Boolean (o)    TRUE | FALSE
+
+empresa = "Casa del código"
+
+//lenguaje de tipado débil
+
+//HERRAMIENTAS JS
+console.log('El nombre de la empresa es: ' + empresa)
+console.warn('Esto es un mensaje de advertencia.')
+console.error('Houston, tenemos un problema.')
+
+// let nombreCompleto = prompt('Ingresa tu nombre de usuario:')
+let resultado = confirm('¿Deseas formatear tu disco?')
+/*
+alert('El nombre de la empresa es: ' + empresa)
+
+prompt('Ingresa tu nombre de usuario:')
+
+confirm('¿Deseas formatear tu disco?')
+*/
